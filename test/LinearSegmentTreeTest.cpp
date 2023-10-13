2166f7eba8d649b74ab9395454cf2d7804efda42
conko <dingjun-work@outlook.com>
2023-10-14 03:00:18 +0800

    update segment_tree.hpp, add basic test for linear_segment_tree

--- test/LinearSegmentTreeTest.cpp
+++ test/LinearSegmentTreeTest.cpp
@@ -5,9 +5,10 @@
 #include "../ds/segment_tree.hpp"
 #include <gtest/gtest.h>
 #include <vector>
-#include <print>
+#include <ranges>
+#include <queue>
 
-class LinearSegmentTreeTestSuite : ::testing::Test {
+class LinearSegmentTreeTestSuite : public inflate::linear_segment_tree<int>, public ::testing::Test{
 
 };
 
@@ -16,4 +17,24 @@ TEST(LinearSegmentTreeTestSuite, ConstructionFromIteratorRangeTest) {
     inflate::linear_segment_tree<int> tree(a.begin(), a.end());
     ASSERT_EQ(tree.size(), a.size());
     ASSERT_EQ(tree.allocation_size(), a.size()*4);
+    ASSERT_EQ(tree.root_node().sum, 55);
+    auto node = reinterpret_cast<const inflate::segment_tree_node<int, int>*>(& (tree.root_node()));
+    ASSERT_EQ(node[1].sum, 15);
+    ASSERT_EQ(node[2].sum, 40);
+    ASSERT_EQ(node[3].sum, 3);
+    ASSERT_EQ(node[4].sum, 12);
+    std::queue<int> q;
+    q.push(1);
+    while (!q.empty()) {
+        const auto& n = q.front();
+        if (!node[n-1].is_leaf()) {
+            ASSERT_EQ(node[n - 1].sum, node[2 * n - 1].sum + node[2 * n].sum);
+            ASSERT_EQ(node[n - 1].max, std::max(node[2 * n - 1].max, node[2 * n].max));
+            ASSERT_EQ(node[n - 1].min, std::min(node[2 * n - 1].min, node[2 * n].min));
+            q.push(2*n);
+            q.push(2*n + 1);
+        }
+        q.pop();
+    }
+
 }
